6fc96f5d021ee03992de241d733a75ac3e4885fb
anjan1972 <anjan_devroy@yahoo.com>
2016-09-27 11:41:33 +1000

    Added all project files

---
+++ Gapminder/R.folder/USQ_SWC.r
@@ -0,0 +1,25 @@
+1 +100 
+100
+3+5*2
+(3+5)+*+2=
+  (3+5)*2
+1 != 2
+3 != 5
+5!=5
+3+<+=-2+
+X <-100
+x
+x <- 1/40
+x <-1/50
+x=x=1
+x = 1/40
+1:5
+2^(1:5)
+ls()
+ls
+rm(x)
+rm(list=ls)
+rm(list
+   install.packages()
+   installed.packages()
+   
